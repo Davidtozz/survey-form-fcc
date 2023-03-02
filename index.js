191e1c26e4a84d5737b97914b566e5781ff535cd
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2023-03-02 22:06:09 +0100

    Page completed, time for some fancy CSS

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-
-
-function myFunction() {
+function dropdownAnimation() {
     dropDownMenu = document.getElementById("dpwn");
     dropDownMenu.classList.toggle("dropdown")
 } 
+
+function toggleTextArea(){
+    other = document.getElementById("other-checkbox");
+    textArea = document.getElementById("feedback-area");
+    if(other.checked && textArea.style.display === "none") {
+        textArea.style.display = "block";
+      } else {
+        textArea.style.display = "none";
+      }
+}
